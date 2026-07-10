6f2d29646bd644e31c28399fe0b2172837ee1795
Johanmby <johanmby@gmail.com>
2026-07-10 13:57:06 -0500

    Arreglando el problema del deploy x2

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   base: '/Website-by-JesusMolina/',
+  plugins: [react()],
 })
